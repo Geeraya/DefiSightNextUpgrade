d06b40bbdb8fe874b2d53f5fd786257b855bfb2c
Mantas Imbrasas <mantasimbrasas@gmail.com>
2023-05-25 21:41:42 +0300

    changes to the router

--- next/src/server/api/routers/wallet.ts
+++ next/src/server/api/routers/wallet.ts
@@ -1,21 +1,44 @@
-import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  publicProcedure,
-  privateProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
 
-export const userRouter = createTRPCRouter({
+export const walletRouter = createTRPCRouter({
   add: privateProcedure
     .input(
-      z.object({ address: z.string(), name: z.string(), higlight: z.string() })
+      z.object({
+        walletAddress: z.string(),
+        walletTag: z.string(),
+        walletHighlight: z.enum(["red", "green", "blue", "yellow"]),
+        walletChainId: z.number(),
+      })
     )
-    .query(({ ctx }) => {
-      return {
-        greeting: `Hello ${ctx.userId}`,
-      };
+    .mutation(async ({ input, ctx }) => {
+      const res = await ctx.trackerService.add.query({
+        ...input,
+        userId: ctx.userId,
+      });
+
+      if (!res || res.status !== "success") {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to add wallet",
+        });
+      }
+
+      await ctx.prisma.wallet.create({
+        data: {
+          address: input.walletAddress,
+          tag: input.walletTag,
+          highlight: input.walletHighlight,
+          chainId: input.walletChainId,
+          user: {
+            connect: {
+              id: ctx.userId,
+            },
+          },
+        },
+      });
+      return { status: "success" };
     }),
 });
